e136bc02b142364eaf6f33bc6c75edc70cfc3388
Santiago Ariel <santiagoarielv98@gmail.com>
2024-07-22 21:27:25 -0300

    chore: Update App component to use Material-UI theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
+import { useMemo } from "react"
 import { useAppDispatch, useAppSelector } from "./app/hooks"
 import { selectColorMode, toggleColorMode } from "./features/themes/themeSlice"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import GlobalStyles from "@mui/material/GlobalStyles"
+import CssBaseline from "@mui/material/CssBaseline"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
 
 const App = () => {
   const dispatch = useAppDispatch()
   const colorMode = useAppSelector(selectColorMode)
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: colorMode,
+        },
+      }),
+    [colorMode],
+  )
 
   return (
-    <div>
-      <button
-        aria-label="Toggle dark mode"
-        onClick={() => dispatch(toggleColorMode())}
-        style={{
-          backgroundColor: colorMode === "dark" ? "#282c34" : "#fff",
-          color: colorMode === "dark" ? "#fff" : "#282c34",
+    <>
+      <GlobalStyles
+        styles={{
+          ":root": {
+            scrollBehavior: "smooth",
+            textRendering: "optimizeLegibility",
+            WebkitFontSmoothing: "antialiased",
+            MozOsxFontSmoothing: "grayscale",
+            textWrap: "balance",
+          },
+          "html,body,#root": {
+            height: "100%",
+            maxWidth: "100%",
+          },
         }}
-      >
-        {colorMode === "dark" ? "🌞" : "🌜"}
-      </button>
-    </div>
+      />
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            bgcolor: "background.default",
+            color: "text.primary",
+            height: "100%",
+          }}
+        >
+          <Button
+            aria-label="Toggle dark mode"
+            onClick={() => dispatch(toggleColorMode())}
+            style={{
+              backgroundColor: colorMode === "dark" ? "#282c34" : "#fff",
+              color: colorMode === "dark" ? "#fff" : "#282c34",
+            }}
+          >
+            {colorMode === "dark" ? "🌞" : "🌜"}
+          </Button>
+        </Box>
+      </ThemeProvider>
+    </>
   )
 }
 
