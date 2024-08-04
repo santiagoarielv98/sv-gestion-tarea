908b34af4e394e40d0b0f2adeac060022d4ba202
Santiago Ariel <santiagoarielv98@gmail.com>
2024-08-04 11:24:36 -0300

    refactor: Remove unused imports and code in Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,11 @@
-import List from "@mui/material/List"
 import Header from "../../components/Header"
 import TaskForm from "../../components/TaskForm"
-import TaskItem from "../../components/TaskItem"
-import { useAppSelector } from "../../redux/hooks"
-import { selectLabelState } from "../../redux/labels/labelSlice"
-import { selectTaskState } from "../../redux/tasks/taskSlice"
 
 function Home() {
-  const { labels } = useAppSelector(selectLabelState)
-  const { tasks } = useAppSelector(selectTaskState)
-
   return (
     <div>
       <Header />
-
       <TaskForm />
-      <List>
-        {tasks.map(task => (
-          <TaskItem key={task.id} task={task} />
-        ))}
-      </List>
-      {/* <pre>{JSON.stringify(tasks, null, 2)}</pre> */}
-
-      <pre>{JSON.stringify(labels, null, 2)}</pre>
     </div>
   )
 }
