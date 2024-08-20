bb29f0c12d440d89b7ae7e138fc6b087600633fd
Santiago Ariel <santiagoarielv98@gmail.com>
2024-08-20 11:38:52 -0300

    refactor: Update package.json and package-lock.json versions to 0.0.2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'react'],
+  plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
   }
 };
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { store } from '@/app/store';
 import { Provider } from 'react-redux';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <Provider store={store}>
