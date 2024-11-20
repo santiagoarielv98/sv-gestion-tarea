ddfc2ac1ca8db1439ba058885cdd6e9be529f5e4
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-20 19:34:29 -0300

    feat: Add profile fetching functionality and refactor authentication hooks

--- src/users/hooks/useAuth.tsx
+++ src/users/hooks/useAuth.tsx
@@ -1,7 +1,16 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { login } from "../services/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchProfile, login } from "../services/api";
 import { taskQueryKey } from "@/tasks/hooks/useTasks";
 import { toast } from "@/hooks/use-toast";
+
+export const authQueryKey = ["auth"];
+
+export function useProfile() {
+  return useQuery({
+    queryKey: authQueryKey,
+    queryFn: fetchProfile,
+  });
+}
 
 export function useLogin() {
   const queryClient = useQueryClient();
--- src/users/services/api.ts
+++ src/users/services/api.ts
@@ -1,22 +1,12 @@
-// import { apiUrl } from "@/common/constants/app";
-// import { Login } from "../schema/auth-schema";
-
 import { api } from "@/common/constants/api";
 import { Login } from "../schema/auth-schema";
 
-// export const login = async (data: Login) => {
-//   const response = await fetch(`${apiUrl}/auth/login`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   });
-
-//   return response.json();
-// };
-
 export const login = async (data: Login) => {
   const response = await api.post("/auth/login", data);
+  return response.data;
+};
+
+export const fetchProfile = async () => {
+  const response = await api.get("/profile");
   return response.data;
 };
