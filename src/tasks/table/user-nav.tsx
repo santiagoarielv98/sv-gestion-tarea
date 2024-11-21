c8fb43fa5ebf507662fd762431b350d933b7dc3a
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-21 11:55:36 -0300

    feat: Enhance user navigation by displaying user name and email; improve avatar fallback

--- src/tasks/table/user-nav.tsx
+++ src/tasks/table/user-nav.tsx
@@ -3,43 +3,54 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/hooks/use-auth";
 import { useLogout } from "@/users/hooks/use-user";
+import { User } from "lucide-react";
 
 export function UserNav() {
+  const { user } = useAuth();
   const { mutate: logout } = useLogout();
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-9 w-9">
             <AvatarImage src="/avatars/03.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            {user.name ? (
+              <AvatarFallback>{user.name[0]}</AvatarFallback>
+            ) : (
+              <User className="h-6 w-6 m-auto" />
+            )}
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">{user.name}</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {user.email}
             </p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
+        {/* <DropdownMenuGroup> */}
+        {/* <DropdownMenuItem>
             Perfil
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          {/* <DropdownMenuItem>
+          </DropdownMenuItem> */}
+        {/* <DropdownMenuItem>
               Billing
               <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
             </DropdownMenuItem>
@@ -47,9 +58,9 @@ export function UserNav() {
               Settings
               <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
             </DropdownMenuItem> */}
-          {/* <DropdownMenuItem>New Team</DropdownMenuItem> */}
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
+        {/* <DropdownMenuItem>New Team</DropdownMenuItem> */}
+        {/* </DropdownMenuGroup> */}
+        {/* <DropdownMenuSeparator /> */}
         <DropdownMenuItem onClick={() => logout()}>
           Logout
           <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
