573ae637213909f5bf400823eaf4b6a53b94a09e
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-25 21:37:56 -0300

    refactor: Remove TagTable and TaskTable components; integrate their logic directly into Tags and Tasks components

--- src/tags/pages/Tags.tsx
+++ src/tags/pages/Tags.tsx
@@ -1,7 +1,25 @@
-import TagTable from "./TagTable";
+import { useTags } from "../hooks/useTags";
+import DataTable from "../table/data-table";
+import { columns } from "../table/columns";
+import React from "react";
+import { toast } from "@/hooks/use-toast";
 
-function Tags() {
-  return <TagTable />;
+function TagTable() {
+  const { data: tasks = [], error } = useTags();
+
+  React.useEffect(() => {
+    if (error) {
+      (error as unknown as Response)?.json?.().then(({ message }) => {
+        toast({
+          title: "Error",
+          description: message ?? "No se pudieron cargar las etiquetas",
+          variant: "destructive",
+        });
+      });
+    }
+  }, [error]);
+
+  return <DataTable data={tasks} columns={columns} />;
 }
 
-export default Tags;
+export default TagTable;
--- src/tasks/pages/Tasks.tsx
+++ src/tasks/pages/Tasks.tsx
@@ -1,7 +1,25 @@
-import TaskTable from "./TaskTable";
+import { useTasks } from "../hooks/useTasks";
+import DataTable from "../table/data-table";
+import { columns } from "../table/columns";
+import React from "react";
+import { toast } from "@/hooks/use-toast";
 
-function Tasks() {
-  return <TaskTable />;
+function TaskTable() {
+  const { data: tasks = [], error } = useTasks();
+
+  React.useEffect(() => {
+    if (error) {
+      (error as unknown as Response)?.json?.().then(({ message }) => {
+        toast({
+          title: "Error",
+          description: message ?? "No se pudieron cargar las tareas",
+          variant: "destructive",
+        });
+      });
+    }
+  }, [error]);
+
+  return <DataTable data={tasks} columns={columns} />;
 }
 
-export default Tasks;
+export default TaskTable;
