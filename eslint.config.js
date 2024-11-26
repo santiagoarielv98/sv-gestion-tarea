ec0ed7f40c2478fd50df0c894ecadbaf7a4bac80
Santiago Ariel <santiagoarielv98@gmail.com>
2024-11-25 22:04:18 -0300

    feat: Update ESLint configuration and add Prettier integration; modify VSCode settings and package dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import pluginQuery from '@tanstack/eslint-plugin-query'
+import js from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2024,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
   },
-  ...pluginQuery.configs['flat/recommended'],
-)
+  ...pluginQuery.configs["flat/recommended"],
+  eslintPluginPrettier,
+);
